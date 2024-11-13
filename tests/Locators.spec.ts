2344bfc99a51d81be4ca7dd9593d2995fa824c68
unnath123 <unnathpgowda@gmail.com>
2024-11-13 18:47:56 +0530

    soft assertion

--- tests/Locators.spec.ts
+++ tests/Locators.spec.ts
@@ -1,21 +1,30 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test("locators", async({page})=>{
+test("locators", async ({ page }) => {
 
     await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/register");
 
-    await  page.locator("id=input-firstname").fill("Unnath");
+    await page.locator("id=input-firstname").fill("Unnath");
 
-    await  page.fill("#input-lastname", "Gowda");
+    await page.fill("#input-lastname", "Gowda");
 
     const formEles = await page.$$(".form-group")
 
     console.log(formEles.length)
 
-    await page.getByRole('link', { name:'sign up'}).fill("unnathvarshi@gmail.com");
-    
+    await  page.waitForTimeout(2000);
 
-    await  page.waitForTimeout(3000);
+    // await page.getByRole('link', { name:'login page'}).click();
+
+    // await page.getByText("Login", {exact: true}).click()
+
+    // await page.getByAltText('naveenopencart').click();
+
+    await expect.soft(page.getByRole('heading', { name: 'Register Accrount' })).toBeVisible();
+
+    await page.locator('#content p a').click();
+
+    await page.waitForTimeout(2000);
 })
 
 // npx playwright test --project=chromium --headed
