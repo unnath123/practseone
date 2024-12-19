9817b3dbe6def068ed7848e3a17bfae7467ff0bc
Unnath Gowda <64188873+unnath123@users.noreply.github.com>
2024-12-20 00:45:55 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,11 +35,11 @@ export default defineConfig({
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    }
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
     /* Additional project configurations can go here */
   ],
 
