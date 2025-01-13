f0b3730c0b25c0171069bc386d569b9efd844222
unnath123 <unnathpgowda@gmail.com>
2025-01-13 23:42:08 +0530

    modules in

--- tests/apiTest.spec.ts
+++ tests/apiTest.spec.ts
@@ -1,4 +1,5 @@
-const { test, expect } = require('@playwright/test');
+// const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test'
 
 test.only("API test", async ({ request }) => {
 
